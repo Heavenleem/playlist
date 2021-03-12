df3455bbc2beb3e0ce8e29c135267df6418a4d68
Glitch (early-oasis-professor) <none>
2021-03-12 01:48:16 +0000

    🚶💝 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+let songs =  [
+  {
+    name: "Daynamite",
+    artist: "BTS",
+    length:0,
+    image:  "https://www.gannett-cdn.com/presto/2020/09/02/USAT/230e4ee9-0e0d-4f8e-b32a-85fbe0d3550e-BTS.jpg?width=1320&height=880&fit=crop&format=pjpg&auto=webp",
+  },
+  {
+    name: "ON",
+    artist: "BTS",
+    length:1,
+    image:"https://www.gannett-cdn.com/presto/2020/09/02/USAT/230e4ee9-0e0d-4f8e-b32a-85fbe0d3550e-BTS.jpg?width=1320&height=880&fit=crop&format=pjpg&auto=webp",
+  },
+  {
+    name: "Lovesickgirl",
+    artist: "Blackpink",
+    length:2,
+    image:  "https://fashionista.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTc2OTM1MjU0MjE5NTY0ODM1/blackpink-k-pop-fashion-style.jpg",
+  },
+  {
+    name: "savage",
+    artist: "Blackpink",
+    length:3,
+    image:  "https://fashionista.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTc2OTM1MjU0MjE5NTY0ODM1/blackpink-k-pop-fashion-style.jpg",
+  }
+];
+for (let song of songs) {
+  console.log(song);
+  $(".kpop").append(` <div >${song["name"]}</div>`);
+  $(".kpopArtist").append(` <div >${song["artist"]}</div>`);
+  $(".kpoplength").append(` <div >${song["length"]}</div>`);
+  $(".kpopimage").append(`<img src="${song["image"]}">`);
+  
+};
+
+
+
+
+
+/*
 let kpopSongs = ["Daynamite", "ON", "Lovesickgirl", "savage"];
 for (let name of kpopSongs) {
   $(".kpop").append(` <div >${name}</div>
@@ -42,7 +82,7 @@ $(".addButton").click(function() {
 
 
 });
-
+*/
 
 
 
